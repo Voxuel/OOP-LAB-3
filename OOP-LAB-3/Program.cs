11978d9e884883009ebc9ca144301f42b291c49e
Voxuel <Leo.Fridh@hotmail.com>
2022-10-30 12:13:04 +0100

    Added3 pointers to baseclass in Main() calls

--- OOP-LAB-3/Program.cs
+++ OOP-LAB-3/Program.cs
@@ -6,6 +6,18 @@ namespace OOP_LAB_3
     {
         static void Main(string[] args)
         {
+            // Creates a new object of Geo-class and the points base class from childclasses.
+            var obj = new Geometri();
+            obj = new Circle();
+            Console.WriteLine($"Area of the circle is : {obj.Area()}");
+
+            obj = new Rectangle();
+            Console.WriteLine($"Area of the rectangle is : {obj.Area()}");
+
+            obj = new Square();
+            Console.WriteLine($"Area of the square is : {obj.Area()}");
+
+            Console.ReadKey();
 
         }
     }
